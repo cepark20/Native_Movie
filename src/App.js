9a85f1af8a21c7c38845dce6efa500eae1af4da4
Jane Park <cepark.dev@gmail.com>
2020-05-26 01:54:54 +0900

    Fetching Movies from API

--- src/App.js
+++ src/App.js
@@ -1,84 +1,26 @@
 import React from 'react';
-import PropTypes from "prop-types"
+import axios from "axios"
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-    "http://aeriskitchen.com/wp-content/upload/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 5
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image: 
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx",
-    rating: 3
-  }
-]
-
-
-function Food({name, picture, rating}){
-  return (
-  <div>
-    <h2>I like {name}</h2>
-    <h4>{rating}/5.0</h4>
-    <img src={picture} alt={name} />
-  </div> 
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number
-}
-
-//setState를 할 때마다 새로운 state + render function 호출 
 class App extends React.Component{
 
   state = {
-    count: 0
-  };
-  add = () => {
-    //this.state.count = 1; not working! 
-    //this.setState({count: this.state.count + 1}); not good
-    this.setState(current => ( //current: current state 
-      {count: current.count + 1}
-    ));
-  }; // 외부의 상태에 의존하지 않는 가장 좋은 방법 
-  minus = () => {
-    this.setState(current => (
-      {count: current.count - 1}
-    ));
+    isLoading: true,
+    movies: []
   };
 
-  // 처음 만들어졌을 때 
+  //async키워드를 사용해야 await 가능 
+  getMovies = async() => {
+      const movies = await axios.get("https://yts.mx/api/v2/list_movies.json");
+  };
+
   componentDidMount(){
-    console.log("component rendered");
-    
-  }
-
-  //업데이트할 때 마다 호출 
-  componentDidUpdate(){
-    console.log("I just updated");
-    
-  }
-
-  componentWillUnmount(){
-    console.log("Goodbye");
+    this.getMovies();
   }
 
   render(){
-    console.log("Im rendering");
-    
+    const{isLoading} = this.state;
     return (
-      <div>
-        <h1>The number is {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
+      <div>{isLoading ? "Loading.." : "We are Ready"}</div>
     );
   }
 }
