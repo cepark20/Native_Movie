beeaf7f58bbbd17fb67dba059ae59378d9c9f4e8
Jane Park <cepark.dev@gmail.com>
2020-06-01 19:21:54 +0900

    Building the Router

--- src/App.js
+++ src/App.js
@@ -1,59 +1,14 @@
 import React from 'react';
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css";
+import {HashRouter, Route} from 'react-router-dom';
+import Home from "./routes/Home";
+import About from "./routes/About";
 
+function App() {
+    return <HashRouter>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
 
-class App extends React.Component{
-
-  state = {
-    isLoading: true,
-    movies: []
-  };
-
-  //async키워드를 사용해야 await 가능 
-  getMovies = async() => {
-      const{
-        data: { 
-          data :{movies}
-        }
-      } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating"); //es6 사용 
-      //const movies = await axios.get("https://yts.mx/api/v2/list_movies.json");
-      //console.log(movies.data.data.movies);
-      //console.log(movies);
-      this.setState({movies, isLoading:false}); 
-  };
-
-  componentDidMount(){
-    this.getMovies();
-  }
-
-  render(){
-    const{isLoading, movies} = this.state;
-    return (
-      <section className = "container">
-      {isLoading ? (
-        <div className="loader">
-          <span className="loader__text">Loading..</span>
-         </div>
-      ) : (
-        <div className="movies">
-          {movies.map(movie => (
-            <Movie 
-              key = {movie.id}
-              id={movie.id} 
-              year={movie.year} 
-              title={movie.title} 
-              genres={movie.genres}
-              summary={movie.summary} 
-              poster={movie.medium_cover_image}
-            />
-          ))}
-        </div>
-      )}
-      </section>
-    );
-  }
+    </HashRouter>;
 }
 
 export default App;
